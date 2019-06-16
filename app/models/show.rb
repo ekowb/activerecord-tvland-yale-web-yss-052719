0a89009d0b3a17a5ad923d74adff052df08bf6e5
github username <github email address>
2019-06-16 09:28:32 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def build_network(net_name)
+        self.network = Network.create(net_name)
+    end
+
 end
